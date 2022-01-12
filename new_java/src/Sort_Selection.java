dd2b04755b4699893d0e2221567bb1f56ec0d92d
worachote1 <timeworachote2002@gmail.com>
2022-01-13 00:41:09 +0700

    Selection

--- new_java/src/Sort_Selection.java
+++ new_java/src/Sort_Selection.java
@@ -1,9 +1,40 @@
 
 public class Sort_Selection {
 
+	public static void display(int a[]) {
+		for (int i = 0; i < a.length; i++) {
+			if (i == 0)
+				System.out.print("{");
+			if (i != a.length - 1)
+				System.out.print(a[i] + ", ");
+			if (i == a.length - 1)
+				System.out.print(a[i] + "}");
+		}
+	}
+
+	public static void selection_sort(int a[]) {
+		
+		for(int i=0;i<a.length;i++)
+		{
+			int min_index = i;
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[min_index]>a[j])
+				{
+					min_index=j;
+				}
+			}
+			int t = a[i];
+			a[i] = a[min_index];
+			a[min_index] = t; 
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("tisama");
+		int arr[] = { 6, 4, 5, 69, 12, 2, 11, 9 };
+		selection_sort(arr);
+		display(arr);
 	}
 
 }
