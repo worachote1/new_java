d738351f17b28ad8d25413ccb9fc16d1b2ad07d8
worachote1 <timeworachote2002@gmail.com>
2022-01-12 17:48:59 +0700

    ads

--- new_java/src/Sort_Bubble.java
+++ new_java/src/Sort_Bubble.java
@@ -1,8 +1,9 @@
-package Learn_Sort;
 
-public class Bubble_Sort {
+public class Sort_Bubble {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Git4");
+
 	}
+
 }
