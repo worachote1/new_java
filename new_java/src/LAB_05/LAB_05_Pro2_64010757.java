f1c49953e00e9d28c2e79c1d1048d8c7dc69de5a
worachote1 <timeworachote2002@gmail.com>
2022-02-06 23:19:22 +0700

    Revise LAB05 using recursive

--- new_java/src/LAB_05/LAB_05_Pro2_64010757.java
+++ new_java/src/LAB_05/LAB_05_Pro2_64010757.java
@@ -1,18 +1,29 @@
 package LAB_05;
 
 public class LAB_05_Pro2_64010757 {
-	public static void displayPerimeterAndArea(RegularPolygon r)
+	public static void displayPerimeterAndArea(RegularPolygon r[],int num)
 	{
-		System.out.println(r.getPerimeter()+" "+r.getArea());
+		int i = 3-num;
+		if(num > 0)
+		{
+			System.out.println(r[i].getPerimeter()+" "+r[i].getArea());
+			num -= 1;
+			displayPerimeterAndArea(r, num);
+		}
+		return;
+		
 	}
 	public static void main(String[] args) {
-		RegularPolygon r1 = new RegularPolygon();
-		RegularPolygon r2 = new RegularPolygon(6,4);
-		RegularPolygon r3 = new RegularPolygon(10,4,5.6,7.8);
+		RegularPolygon r[] = new RegularPolygon[3];
+		 r[0] = new RegularPolygon();
+		 r[1] = new RegularPolygon(6,4);
+		 r[2] = new RegularPolygon(10,4,5.6,7.8);
 	
-		displayPerimeterAndArea(r1);
-		displayPerimeterAndArea(r2);
-		displayPerimeterAndArea(r3);
+		 displayPerimeterAndArea(r, r.length);
+		 
+//		displayPerimeterAndArea(r1);
+//		displayPerimeterAndArea(r2);
+//		displayPerimeterAndArea(r3);
 	}
 
 }
