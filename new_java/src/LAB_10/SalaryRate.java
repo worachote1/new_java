e355f9a2222798fb0b08cc5b9745a0066bad1f6f
worachote1 <timeworachote2002@gmail.com>
2022-04-05 13:36:39 +0700

    e

--- new_java/src/LAB_10/SalaryRate.java
+++ new_java/src/LAB_10/SalaryRate.java
@@ -6,7 +6,7 @@ public class SalaryRate {
 	the range from 50,000 to 80,000, for associate professor from 60,000 to 110,000, and
 	for full professor from 75,000 to 130,000.
 	*/
-	public static double ASSISTANT_MIN = 50000, ASSISTANT_MAX = 80000;
-	public static double ASSOCIATE_MIN = 60000, ASSOCIATE_MAX = 110000;
-	public static double FULL_MIN = 75000	  , FULL_MAX = 130000;
+	public final static double ASSISTANT_MIN = 50000, ASSISTANT_MAX = 80000;
+	public final static double ASSOCIATE_MIN = 60000, ASSOCIATE_MAX = 110000;
+	public final static double FULL_MIN = 75000	  , FULL_MAX = 130000;
 }
