7ef56a002d34ec8499b4fff519e946154a6e623a
worachote1 <timeworachote2002@gmail.com>
2022-04-06 22:11:23 +0700

    Commit LAB 11 add save and load event

--- new_java/src/LAB_11/LAB_11_Pro1_64010757.java
+++ new_java/src/LAB_11/LAB_11_Pro1_64010757.java
@@ -1,5 +1,12 @@
 package LAB_11;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import javafx.application.Application;
@@ -9,6 +16,8 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -17,8 +26,10 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class LAB_11_Pro1_64010757 extends Application {
@@ -28,7 +39,7 @@ public class LAB_11_Pro1_64010757 extends Application {
 	   
 	   @Override
 	   public void start(Stage primaryStage) {
-		   primaryStage.setTitle("Registration Form JavaFX Application");
+		   primaryStage.setTitle("LAB_11_Pro1_64010757");
 		// Instantiate a new Grid Pane
 	        GridPane gridPane = new GridPane();
 
@@ -44,9 +55,7 @@ public class LAB_11_Pro1_64010757 extends Application {
 	        // Set the vertical gap between rows
 	        gridPane.setVgap(10);
 
-	    
-	        
-	        
+	      
 	        //Label Section
 
 	        // #1 Add Label
@@ -126,8 +135,33 @@ public class LAB_11_Pro1_64010757 extends Application {
             EventHandler<ActionEvent> save_event = new EventHandler<ActionEvent>() {
                 public void handle(ActionEvent e)
                 {
-                	//System.out.println("test save");
-                	
+        			
+                	try {
+						DataOutputStream output =  new DataOutputStream(new FileOutputStream("C:\\Users\\User PC\\git\\new_java\\new_java\\src\\LAB_11\\saveBinary_data.dat"));
+						
+						String values[] = {
+								Annual_Field.getText(),
+								year_Field.getText(),
+								loan_Field.getText(),
+								monthly_Field.getText(),
+								total_Field.getText()
+								};
+						byte c = 0;
+						String ss="";
+						for(String value : values)
+						{
+							ss += value; c++;
+							if(c!=values.length)
+							{
+								ss += ",";
+							}
+						}		        
+						output.writeUTF(ss);
+				        output.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}	
                 }
             };
             //load_event
@@ -136,6 +170,54 @@ public class LAB_11_Pro1_64010757 extends Application {
                 {
                 	 //System.out.println("test load");
                 	
+                	try {
+						DataInputStream input = new DataInputStream(new FileInputStream("C:\\Users\\User PC\\git\\new_java\\new_java\\src\\LAB_11\\saveBinary_data.dat"));
+				        String ss="";
+						while (input.available() > 0) 
+						{
+				             ss = input.readUTF();
+				            System.out.println("Reading from a Binary File : "+ss);
+				        }
+						
+						//String load
+						String load_string[] = ss.split(",");
+						System.out.println(Arrays.toString(load_string));
+						
+						//check to clear before load data
+						if(!Annual_Field.getText().isEmpty() || 
+						   !year_Field.getText().isEmpty() || 
+						   !loan_Field.getText().isEmpty() ||
+						   !monthly_Field.getText().isEmpty() ||
+						   !total_Field.getText().isEmpty()
+						   )
+						{
+							// not clear Field before
+							Alert alert = new Alert(AlertType.WARNING);
+						 	alert.setContentText("Please clear text Field before load data");
+	                    	alert.show();
+							
+						}
+						else
+						{
+							//Clear Field already
+							String load_annual = load_string[0];
+							String load_year = load_string[1];
+							String load_loan = load_string[2];
+							String load_month = load_string[3];
+							String load_total = load_string[4];
+							
+							//Set each text field by click load button
+			             	Annual_Field.setText(load_annual);
+		                	year_Field.setText(load_year);
+		                	loan_Field.setText(load_loan);
+		                	monthly_Field.setText(load_month);
+		                	total_Field.setText(load_total);
+		                	
+						}
+                	} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
                 }
             };
             //clear_event
@@ -155,19 +237,63 @@ public class LAB_11_Pro1_64010757 extends Application {
                 public void handle(ActionEvent e)
                 {
                     //System.out.println("test calculate");
-                	double interestRate = Double.parseDouble(Annual_Field.getText());
-                	int year = Integer.parseInt(year_Field.getText());
-                	double loan = Double.parseDouble(loan_Field.getText());
+             
+        
+                	//Check text
+                	Boolean checkText = true;
                 	
-                	double r = interestRate / 1200;
-                	int n = year * 12;
+                	Alert alert = new Alert(AlertType.WARNING);
                 	
-                	double monthlyPayment = (loan * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
-                	double totalPayment =  monthlyPayment * 12 * year;
-                
+                	ArrayList<String> alert_text = new ArrayList<String>();
+                	if(Annual_Field.getText().isEmpty() || year_Field.getText().isEmpty() || loan_Field.getText().isEmpty() )
+                	{
+                		if(Annual_Field.getText().isEmpty())
+                		{
+                			alert_text.add("Annual Interest Rate");
+                		}
+                		if(year_Field.getText().isEmpty()) 
+                		{
+                			alert_text.add("Number of Years");
+                		}
+                		if(loan_Field.getText().isEmpty())
+                		{
+                			alert_text.add("Loan Amount");
+                		}
+                		
+                		String ss="";
+                		for(int i=0;i<alert_text.size();i++)
+                		{
+                			ss += alert_text.get(i);
+                			if(i!=alert_text.size()-1)
+                			{
+                				ss += " , ";
+                			}
+                		}
+                    	alert.setContentText("Please enter "+ss);
+                    	alert.show();
+                    	
+                    	checkText = false;
+                	}
+                	
+                	//System.out.println("test length : "+loan_Field.getText().length());
+                	
                 	//display
-                	monthly_Field.setText(String.valueOf(String.format("$%.2f", monthlyPayment)));
-                	total_Field.setText(String.valueOf(String.format("$%.2f", totalPayment)));
+                	if(checkText)
+                	{
+                    	double interestRate = Double.parseDouble(Annual_Field.getText());
+                    	int year = Integer.parseInt(year_Field.getText());
+                    	double loan = Double.parseDouble(loan_Field.getText());
+                    	
+                    	double r = interestRate / 1200;
+                    	int n = year * 12;
+                    	
+                    	double monthlyPayment = (loan * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1);
+                    	double totalPayment =  monthlyPayment * 12 * year;
+                    	
+                    	monthly_Field.setText(String.valueOf(String.format("$%.2f", monthlyPayment)));
+                    	total_Field.setText(String.valueOf(String.format("$%.2f", totalPayment)));
+                    	
+                	}
                 	
                 }
             };
@@ -178,9 +304,9 @@ public class LAB_11_Pro1_64010757 extends Application {
             calculate_btn.setOnAction(calculate_event);
             
 	        gridPane.setAlignment(Pos.TOP_CENTER);
-		    primaryStage.setScene(new Scene(gridPane, 600, 400 ));
-		        
-		  
+		    primaryStage.setScene(new Scene(gridPane, 604, 444 ));
+		    
 		    primaryStage.show();
+		   
 	   }
 	}
